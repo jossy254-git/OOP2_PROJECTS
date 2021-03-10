4422e866b8eaf734a4a8cd12a9bbf3e544c1ef11
Jossy j <70842962+jossy254-git@users.noreply.github.com>
2021-03-10 16:38:33 +0300

    Create volume_area.py

---
+++ volume_area.py
@@ -0,0 +1,10 @@
+#area of a circle
+def circle(r):
+  area = 22/7*(r**2)
+  print ("the area is: " + str(area))
+circle(10)
+#volume of a cylinder
+def cylinder(r, h):
+  v=22/7*((r**2)*h)
+  print("the volume is: " + str(v))
+cylinder(10, 20)
